44e3514a53231c34feac1227c92b5ed04106caef
Pedrohrsantos01 <pedrohrsantos01@gmail.com>
2023-07-06 16:04:34 -0300

    create SchedulingDetails Screen

--- App.tsx
+++ App.tsx
@@ -16,6 +16,7 @@ import AppLoading from "expo-app-loading";
 import { CarDetails } from "./src/screens/CarDetails";
 import theme from "./src/styles/theme";
 import { Scheduling } from "./src/screens/Scheduling";
+import { SchedulingDetails } from "./src/screens/SchedulingDetails";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -32,7 +33,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Scheduling />
+      <SchedulingDetails />
     </ThemeProvider>
   );
 }
