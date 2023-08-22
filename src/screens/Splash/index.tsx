1a9d6682ddbd3ee0e4748aa223f6490f2e0d6bc9
Pedrohrsantos01 <pedrohrsantos01@gmail.com>
2023-08-22 12:00:25 -0300

    create splash screen

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,6 +1,48 @@
 import React from "react";
 import { Container } from "./styles";
+import { Button, StyleSheet, Dimensions } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+  Easing,
+} from "react-native-reanimated";
+
+const WIDTH = Dimensions.get("window").width;
 
 export function Splash() {
-  return <Container></Container>;
+  const animation = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: withTiming(animation.value, {
+            duration: 500,
+            easing: Easing.bounce,
+          }),
+        },
+      ],
+    };
+  });
+
+  function handleAnimation() {
+    animation.value = Math.random() * (WIDTH - 100);
+  }
+
+  return (
+    <Container>
+      <Animated.View style={[styless.box, animatedStyle]} />
+
+      <Button title="MOVER" onPress={handleAnimation} />
+    </Container>
+  );
 }
+
+const styless = StyleSheet.create({
+  box: {
+    width: 100,
+    height: 100,
+    backgroundColor: "red",
+  },
+});
